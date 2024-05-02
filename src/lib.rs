75edd8a111165674afc4302e9768aa23011cd1a0
Jefferson Carlos <jeff.carlosbd@gmail.com>
2024-05-02 08:56:49 -0300

    exporta tipo

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
-use sqlx::{postgres::PgPoolOptions, Error};
+use sqlx::{postgres::PgPoolOptions, Error, Pool, Postgres};
 
-pub use sqlx::{Pool, Postgres};
+pub type PostgresConnection = Pool<Postgres>;
 
 pub struct PgConnector {
     postgres_url: String,
@@ -10,7 +10,7 @@ impl PgConnector {
         PgConnector { postgres_url }
     }
 
-    pub async fn open_connection(&self) -> Result<Pool<Postgres>, Error> {
+    pub async fn open_connection(&self) -> Result<PostgresConnection, Error> {
         PgPoolOptions::new()
             .max_connections(5)
             .connect(&self.postgres_url)
